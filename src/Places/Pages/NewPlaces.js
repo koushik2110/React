958ed30c13caee45609db747fcf738e64c01b6a9
Koushik M S <koushik.m.s1@ibm.com>
2024-01-15 01:39:10 +0530

    added basic validation

--- src/Places/Pages/NewPlaces.js
+++ src/Places/Pages/NewPlaces.js
@@ -1,9 +1,18 @@
-import Input from '../../shared/Components/FormElements/Input';
-import './NewPlaces.css';
-const NewPlace = ()=>{
-
-    return <form className='place-form'>
-        <Input element='input' type='text' label='Title' placeholder='One-Piece' errorText='please enter correct Title' ></Input>
+import Input from "../../shared/Components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/Components/Utils/Validators";
+import "./NewPlaces.css";
+const NewPlace = () => {
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        placeholder="One-Piece"
+        errorText="please enter correct Title"
+        validators={[VALIDATOR_REQUIRE()]}
+      ></Input>
     </form>
-}
+  );
+};
 export default NewPlace;
